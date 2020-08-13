40ebcf989f703d22be64263b3375377909483518
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-08-13 09:35:51 +0100

    added queryparams example

--- NPM/QueryParams.js
+++ NPM/QueryParams.js
@@ -3,18 +3,24 @@ var URL=require("url")
 let requestprocessing=function(request,response){
 	let myurl=URL.parse(request.url,true)
 	response.writeHead(200, {'Content-Type': 'text/html'});
-	
-	console.log("test")
 	if(myurl.pathname=="/"){
 		for(let a=1;a<=10;a++){
-			response.write("<A href='http://localhost:4000/timestable'>"+a+"</A>");
+			response.write("<A href='http://localhost:4000/timestable?t="+a+"'>"+a+"</A>");
 			response.write("<br>");
 		}
 		response.end();
 	}
 	if(myurl.pathname=="/timestable"){
-		response.write("<A href='http://localhost:4000'> Home </A>")
-		response.write("<h2> Your Times Tabel is "+ myurl.query.t + " </h2>")
+		let t = myurl.query.t;
+		response.write("<A href='http://localhost:4000'> Home </A>");
+		response.write("<h2> Your Times Table is "+ t + " </h2>"); 
+		response.write("<table>");
+		for(let i=1; i<=12; i++){
+			response.write("<tr><td>");
+			response.write(t + "X" + i + "="+(t*i));
+			response.write("</td></tr>");
+		}
+		response.write("</table>");
 		response.end();
 	}
 }
