f45a7e5851a64e3b19730c7e530040785c09f0c6
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-08-13 09:53:59 +0100

    changed to arrow function

--- Express/MultipleRequests.js
+++ Express/MultipleRequests.js
@@ -1,8 +1,8 @@
 let express = require("express");
 let app = express();
 
-app.get(["/one","/two","/three"], function(req,res){
-    console.log("hello friends");
-});
+app.get(["/one","/two","/three"],((req,res)=>{
+   console.log("hello friends");
+}));
 
 app.listen(8078);
