53167a1efce19b090fbdb858f611f8c9440ee427
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-04 12:44:41 +0000

    SV: Added Middleware and Route example

--- sample-code/server.js
+++ sample-code/server.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express(); 
 
+// Middleware
+const logger = (req, res, next) => {
+	console.log(new Date());
+	next();
+}
+
+app.use(logger);
+
+// Route example:
+app.get("/hello", (req,res) => {
+	res.send("Who dat");
+});
+
+app.get("/",logger,(req,res) => {
+	res.send("Hello,world");
+});
+
 const server = app.listen(5019, () => {
-    console.log(`Server listening on port ${server.address().port}`);
+	console.log(`Server listening on port ${server.address().port}`);
 });
