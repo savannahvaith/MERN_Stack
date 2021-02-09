6bdc35f856820276df162e7f8d9b5f5cff394ba2
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-09 11:27:59 +0000

    SV: Removed middleware and unused imports

--- sample-code/server.js
+++ sample-code/server.js
@@ -2,18 +2,8 @@ const express = require('express');
 const app = express(); 
 const productRouter = require("./routes/products");
 const {PRODUCT_URL} = require('./config/config.json');
-// const {trainerUrl} = require("./config/config.json");
-// const trainerRoutes = require("./routes/trainers");
 
 app.use(express.json());
-
-// Middleware
-// const logger = (req, res, next) => {
-// 	console.log(new Date());
-// 	next();
-// }
-
-// app.use(logger);
 
 app.use(PRODUCT_URL,productRouter);
 // app.use(trainerUrl, trainerRoutes);
