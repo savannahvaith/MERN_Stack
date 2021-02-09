0b7b6c5eb58c1ac83e61b89ec8a296b66a4e4532
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-09 15:26:42 +0000

    SV: Added routers to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 // import express and cors
-const express = require("express");
-const cors = require("cors");
-
+const express = require(`express`);
+const cors = require(`cors`);
 const app = express();
 const port = process.env.port || 1905; 
 
-
 app.use(cors());
+
+const todoRouter = require(`./routes/todo`);
+const {TODO_URL} = require(`./config/consts.json`);
+
+app.use(express.json());
+app.use(TODO_URL,todoRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
