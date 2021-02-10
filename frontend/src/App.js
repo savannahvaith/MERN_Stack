15e286b502eebff2a332e5bd0820d0701f58396e
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-10 14:27:41 +0000

    SV: Added Router and Nav Links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import Home from "./Components/HomePage/Home";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from "./Components/HomePage/Nav";
+import './Resources/app.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
+    <div>
+      <Router>
+        <Navigation/>
+        <Route path="/" exact>
+            <Home/>
+        </Route>
+      </Router>
     </div>
   );
 }
