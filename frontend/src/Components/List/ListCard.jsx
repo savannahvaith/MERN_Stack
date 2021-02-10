7d1a0b070314de994c1d5c4c5e8ac190ba50b8be
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-10 15:29:43 +0000

    SV: Made code more modular

--- frontend/src/Components/List/ListCard.jsx
+++ frontend/src/Components/List/ListCard.jsx
@@ -1,14 +1,22 @@
+import DeleteList from "./DeleteList";
+import EditList from "./EditList";
+
 const ListCard = ({id,title}) => {
     return(
         <div className="col-xl-4">
             <div className="card">
                 <div className="card-header teal">
                     <h6 className="col-md-10 text-white title">{title}</h6>
-                    <span>&#8942;</span>
+                    <EditList />
                 </div>
-                <div className="card-body">
+                <div className="card-body" style={{height: "400px"}}>
                 </div>
                 <div className="card-footer">
+                    <div className="float-right">
+                        <DeleteList id={id}/>
+                    {' '}
+                    <button className="btn btn-outline-success">Add Task</button>
+                    </div>
                 </div>
             </div>
             <br/>
