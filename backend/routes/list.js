6aaace89fb6eed00bb8da4a98a547085785bc0a5
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-11 10:05:08 +0000

    SV: Added Error handling with chained function

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -2,48 +2,48 @@ const router = require('express').Router();
 const { List } = require('../config/db');
 
 // CREATE
-router.post(`/create`, ({ body }, res) => {
+router.post(`/create`, ({ body }, res,next) => {
     const todo = new List(body);
     todo.save().then(result => {
         res.status(201).send(`${result.title} addedd successfully!`);
-    }).catch(err => console.error(err));
+    }).catch(err => next(err));
 });
 
 // READ
-router.get(`/getAll`, (req, res) => {
+router.get(`/getAll`, (req, res,next) => {
     List.find((err, result) => {
         if (err) {
-            console.error(err);
+            next(err);
         }
         res.status(200).send(result);
     });
 });
 
 // READ ONE
-router.get(`/get/:id`, (req, res) => {
+router.get(`/get/:id`, (req, res,next) => {
     List.findById(req.params.id, (err, result) => {
         if (err) {
-            console.error(err);
+            next(err);
         }
         res.status(200).send(result);
     });
 });
 
 // UPDATE
-router.patch(`/update/:id`, (req, res) => {
+router.patch(`/update/:id`, (req, res,next) => {
     List.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, result) => {
         if (err) {
-            console.error(err);
+            next(err);
         }
         res.status(202).send(result);
     });
 });
 
 // DELETE
-router.delete(`/delete/:id`, (req, res) => {
+router.delete(`/delete/:id`, (req, res,next) => {
     List.findByIdAndDelete(req.params.id, (err, result) => {
         if (err) {
-            console.error(err);
+            next(err);
         };
         res.status(204).send(result);
     });
--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,27 @@ const express = require(`express`);
 const cors = require(`cors`);
 const app = express();
 const port = process.env.port || 1905; 
-
+const createError = require(`http-errors`);
 app.use(cors());
+app.use(express.json());
+
 
 const todoRouter = require(`./routes/todo`);
 const listRouter = require(`./routes/list`);
 const {TODO_URL} = require(`./config/consts.json`);
 const {LIST_URL} = require(`./config/consts.json`);
 
-app.use(express.json());
 app.use(TODO_URL,todoRouter);
 app.use(LIST_URL,listRouter);
+
+// Error handling if anything fails
+app.use((req,res,next) => {
+    next(createError(404, 'Resource not found'));
+});
+
+app.use((err,req,res,next)=> {
+    res.status(err.statusCode || 500).send(err.message);
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
