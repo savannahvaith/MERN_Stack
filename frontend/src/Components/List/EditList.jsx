9791d2c55861392e551da41cf8cf3df269564383
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-12 18:25:28 +0000

    S: Fixed Edit modal

--- frontend/src/Components/List/EditList.jsx
+++ frontend/src/Components/List/EditList.jsx
@@ -1,57 +1,62 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { LIST_URL } from '../../CONSTS.json';
+import  Modal  from 'react-bootstrap/Modal';
 
-const EditList = ({ id, title,trigger }) => {
+const EditList = ({ listID, trigger, listTitle }) => {
 
-    const [titleUpdate, setTitleUpdate] = useState(title);
+    const [titleUpdate, setTitleUpdate] = useState(listTitle);
+    console.log(`From editlist - ${listID}`);
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const update = (e) => {
         e.preventDefault();
-        axios.patch(`${LIST_URL}/update/${id}`,
-         { title:titleUpdate })
-         .then(res => trigger(`${res.data.title} List Updated!`));
+        axios.patch(`${LIST_URL}/update/${listID}`, { title: titleUpdate }).then((res) => {
+            console.log(res);
+            trigger(`Updated ${res.data.title}`);
+            setTitleUpdate('');
+            document.querySelector('#inputtitle').value = "";
+        });
     }
 
     return (
         <>
             <button type="button" className="btn btn-outline-dark"
-                data-bs-toggle="modal" data-bs-target="#editList">
+                onClick={handleShow}>
                 &#8942;
             </button>
 
-            <div className="modal" id="editList">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h2 className="modal-title">Edit</h2>
-                            <button type="button" data-bs-dismiss="modal" className="btn btn-outline-danger">
-                                <span>X</span>
-                            </button>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header>
+                    <Modal.Title>{listTitle}</Modal.Title>
+                    <button type="button" onClick={handleClose} className="btn btn-outline-danger">
+                        <span>X</span>
+                    </button>
+                </Modal.Header>
+                <form onSubmit={update}>
+                    <Modal.Body>
+                        <div className="form-floating mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Updated Title:"
+                                id="inputtitle"
+                                value={titleUpdate}
+                                onChange={e => setTitleUpdate(e.target.value)}
+                            />
+                            <label htmlFor="inputtitle">Updated Title:</label>
                         </div>
-                        <form onSubmit={update}>
-                            <div className="modal-body">
-                                <div className="form-floating mb-3">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Updated Title:"
-                                        id="update"
-                                        value={titleUpdate}
-                                        onChange={e => setTitleUpdate(e.target.value)}
-                                    />
-                                    <label htmlFor="update">Updated Title:</label>
-                                </div>
 
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" data-bs-dismiss="modal" className="btn btn-outline-danger">Cancel</button>
-                                <button type="submit" onClick={update} data-bs-dismiss="modal" className="btn btn-outline-success">Update</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button type="button" onClick={handleClose} className="btn btn-outline-danger">Cancel</button>
+                        <button type="submit" onClick={handleClose} className="btn btn-outline-success">Update</button>
+                    </Modal.Footer>
+                </form>
+            </Modal>
         </>
     )
 
